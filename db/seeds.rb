8f0c103214389720569fd9cf0dda6c0fc0f390b9
Cory Martin <murribu@gmail.com>
2015-05-12 15:11:43 -0500

    Add seed for default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 o = OauthApplication.create(:name => 'Master', :secret => Rails.application.secrets[:mastersecret], :redirect_uri => 'http://corymmartin.com/callback_game.php')
 o.oauth_access_tokens.create(:token => Rails.application.secrets[:mastertoken], :scope => 'master')
+u = User.create(:email => Rails.application.secrets[:defaultemail], :password => Rails.application.secrets[:defaultpassword], :password_confirmation => Rails.application.secrets[:defaultpassword])
